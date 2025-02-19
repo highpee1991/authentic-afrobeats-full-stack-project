148cfef5adc5915ad33dfa5a1e97048d127411fa
Ipadeola Taiwo <ipadeola.it@gmail.com>
2025-02-20 00:43:37 +0100

    flash sales updated

--- frontend-with-supabase/src/components/ui/forSales/ForSale.jsx
+++ frontend-with-supabase/src/components/ui/forSales/ForSale.jsx
@@ -20,6 +20,7 @@ const Container = styled.div`
 
   @media (max-width: 768px) {
     flex-direction: column;
+    max-width: 400px;
   }
 `;
 
@@ -54,10 +55,13 @@ const Content = styled.div`
   color: #333;
   margin-bottom: 1.5rem;
   line-height: 1.6;
-  max-width: 400px;
 
   div {
     margin-bottom: 0.8rem;
+  }
+
+  @media (max-width: 768px) {
+    max-width: 400px;
   }
 `;
 
