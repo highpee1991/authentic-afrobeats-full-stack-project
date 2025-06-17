76b69740e2c5d8a5ae0742a6e163bce0e27a8e36
Ipadeola Taiwo <ipadeola.it@gmail.com>
2025-06-17 22:19:13 +0100

    refactore best offer header display

--- frontend-with-supabase/src/components/ui/bestOffers/BestOffers.jsx
+++ frontend-with-supabase/src/components/ui/bestOffers/BestOffers.jsx
@@ -15,70 +15,6 @@ const Title = styled.h2`
   margin-bottom: 2rem;
 `;
 
-// const BestOffers = () => {
-//   const { data, isLoading, error } = useQuery({
-//     queryKey: ["discountedProducts"],
-//     queryFn: getDiscountedProducts,
-//   });
-//   const [shuffleProducts, setShuffleProducts] = useState([]);
-
-//   useEffect(() => {
-//     if (data) {
-//       const shuffled = [...data].sort(() => Math.random() - 0.5);
-//       setShuffleProducts(shuffled);
-//     }
-//   }, [data]);
-
-//   if (isLoading) return <Spinner />;
-//   if (error) return <p>Failed to load best selling products</p>;
-
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     // autoplaySpeed: 100,
-//     responsive: [
-//       {
-//         breakpoint: 1024, // At 768px and above, show 3 products per slide
-//         settings: {
-//           slidesToShow: 3,
-//           slidesToScroll: 1,
-//         },
-//       },
-//       {
-//         breakpoint: 768, // At 768px and above, show 3 products per slide
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 1,
-//         },
-//       },
-//       {
-//         breakpoint: 468, // At 480px and above, show 1 product per slide
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-//         },
-//       },
-//     ],
-//   };
-
-//   return (
-//     <BestOffersWrapper>
-//       <Title>Best Offers</Title>
-//       <Slider {...settings}>
-//         {shuffleProducts.map((product) => (
-//           <ProductCard key={product.id} product={product} />
-//         ))}
-//       </Slider>
-//     </BestOffersWrapper>
-//   );
-// };
-
-// export default BestOffers;
-
 //////////////////
 const BestOffers = () => {
   const { data, isLoading, error } = useQuery({
@@ -132,7 +68,7 @@ const BestOffers = () => {
 
   return (
     <BestOffersWrapper>
-      <Title>Best Offers</Title>
+      {shuffleProducts.length >= 1 && <Title>Best Offers</Title>}
       <Slider {...settings}>
         {shuffleProducts.map((product) => (
           <ProductCard
